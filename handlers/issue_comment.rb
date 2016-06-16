9fda5a7211e40f122e082bd803f24604d78c7ad4
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-06-16 10:29:22 +0200

    Issue comments

--- handlers/issue_comment.rb
+++ handlers/issue_comment.rb
@@ -1,6 +1,9 @@
 module RubotHandlers::IssueComment
   def self.handle(payload)
     %(commented on issue #{payload.tiny_issue}
-<#{payload['comment']['html_url']}>)
+<#{payload['comment']['html_url']}>
+```
+#{payload['comment']['body']}
+```)
   end
 end
