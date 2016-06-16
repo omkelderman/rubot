04f6f526e36151429833afe92473d23cd1be24ca
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-06-16 14:26:22 +0200

    PR diff comments

---
+++ handlers/pull_request_review_comment.rb
@@ -0,0 +1,18 @@
+module RubotHandlers::PullRequestReviewComment
+  def self.handle(payload)
+    action = case payload['action']
+    when 'created'
+      'commented'
+    when 'edited'
+      'edited a comment'
+    when 'deleted'
+      'deleted a comment'
+    end
+
+    %(#{action} on pull request #{payload.tiny_pull_request}
+<#{payload['comment']['html_url']}>
+```
+#{payload['comment']['body']}
+```)
+  end
+end
