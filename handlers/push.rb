c79e6d6141ddddaca1b1933e4be4c4bd5ab1078e
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-06-17 12:19:18 +0200

    Commit URLs

--- handlers/push.rb
+++ handlers/push.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
+require 'net/http'
 
 module RubotHandlers::Push
   def self.format_commit(commit)
     sha = "`#{commit['id'][0..6]}` "
     message = commit['message'].lines[0].strip
     author = " [#{commit['author']['username']}]"
-    sha + message + author
+
+    uri = URI('https://git.io')
+    response = Net::HTTP.post_form(uri, {url: commit['url']})
+    url = if response['Location']
+      response['Location']
+    else
+      commit['url']
+    end
+
+    url = " (<#{url}>)"
+    sha + message + author + url
   end
 
   def self.handle(payload)
@@ -15,6 +26,7 @@ module RubotHandlers::Push
       case type
       when 'heads' # branch
         str = "pushed **#{payload['commits'].length}** commit#{payload['commits'].length == 1 ? '' : 's'} to a branch **#{name}**\n"
+        str += "<#{payload['compare']}>\n"
         str += payload['commits'].map { |e| format_commit(e) }.join("\n")
         str
       end
