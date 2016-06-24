c29c3672ec3735459a5b7045a847db89dee55cb3
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2016-06-24 19:42:10 +0200

    Commit comments

---
+++ handlers/commit_comment.rb
@@ -0,0 +1,10 @@
+module RubotHandlers::CommitComment
+  def self.handle(payload)
+    comment = payload['comment'];
+    %(commented on a commit `#{comment['commit_id'][0..6]}`
+<#{comment['html_url']}>
+```
+#{comment['body']}
+```)
+  end
+end
